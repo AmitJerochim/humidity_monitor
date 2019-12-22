5c9cfb816ef97a14abe68e32e99a1f8478a05d03
AmitJerochim <amit.jerochim@gmx.de>
2019-12-22 23:22:10 +0100

    node server can request html files and javascript files

--- node_server.js
+++ node_server.js
@@ -1,11 +1,29 @@
-const http = require('http');
 const bodyParser = require("body-parser");
-
+const fs = require('fs');
+const http = require('http');
 const hostname = '127.0.0.1';
 const port = 3000;
 
 let newData='';
+
 const server = http.createServer((req, res) => {
+if(/^\/[a-zA-Z0-9\/]*.js$/.test(req.url.toString())){
+   sendFileContent(res, req.url.toString().substring(1), "text/javascript");
+}
+ 
+function sendFileContent(res, fileName, contentType){
+  fs.readFile(fileName, function(err, data){
+    if(err){
+      res.writeHead(404);
+      res.write("Not Found!");
+    }
+    else{
+      res.writeHead(200, {'Content-Type': contentType});
+      res.write(data);
+    }
+    res.end();
+  });
+}
   if(req.method==='POST'){
      let body = '';
      req.on('data', function(data){
@@ -21,9 +39,22 @@ const server = http.createServer((req, res) => {
 
   if(req.method==='GET' && req.url==="/"){
      console.log("Data:" + newData)
+     console.log("dirname:" + __dirname)
      res.statusCode = 200;
-     res.setHeader('Content-Type', 'text/plain');
-     res.end(newData);
+     res.setHeader('Content-Type', 'text/html');
+     let path=__dirname+"/view/index.html";
+     console.log("path:" + path)
+     let readStream = fs.createReadStream(path);
+     fs.readFile(path, (err, file)=>{
+       console.log("sent"); 
+       if (err) {console.log(err)};
+       if(file){ console.log(file);
+       res.write(file);
+
+       res.end();}
+    });
+     //readStream.pipe(res);
+     // res.sendFile(newData);
   }
 
   if (req.method==='GET' && req.url==='/data') {
