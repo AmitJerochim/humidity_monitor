37dcd89f388c82a3e6ae19e106e0d7bba8e6183b
AmitJerochim <amit.jerochim@gmx.de>
2019-12-25 22:23:59 +0100

    changed server name

--- send_data.sh
+++ send_data.sh
@@ -6,13 +6,13 @@
 #sensor is not able to measure in periods of less than one second
 #
 
-
+server="192.168.178.55"
 seconds=$1
 seconds+="s"
 echo $seconds
 while :
 do
-	curl --header "Content-Type: application/json"   --request POST   --data "$(python measure_data.py)"   http://localhost:3000/
+	curl --header "Content-Type: application/json"   --request POST   --data "$(python measure_data.py)"   http://$server:3000/
 	sleep $seconds
 done
 
